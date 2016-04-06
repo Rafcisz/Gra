23a10de2385229724130d077ee674e49bd421dcb
Rafcisz <geralt098@gmail.com>
2016-04-06 20:36:15 +0200

    Added files via upload

---
+++ GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public Plansza skryptPlanszy;
+
+    private int level = 5;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        skryptPlanszy = GetComponent<Plansza>();
+        InitGame();
+	}
+
+    void InitGame()
+    {
+        skryptPlanszy.Tworzenie(level);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
